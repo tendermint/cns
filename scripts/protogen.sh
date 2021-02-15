28bd4a59f8034f0f782577a34964dd7b7c692fe4
sahith-narahari <sahithnarahari@gmail.com>
2021-02-16 01:24:55 +0530

    cleanup proto

--- scripts/protogen.sh
+++ scripts/protogen.sh
@@ -2,22 +2,27 @@
 
 set -eo pipefail
 
-PROJECT_PROTO_DIR=proto/cns
 COSMOS_SDK_DIR=${COSMOS_SDK_DIR:-$(go list -f "{{ .Dir }}" -m github.com/cosmos/cosmos-sdk)}
 
-# Generate Go types from protobuf
-protoc \
-  -I=. \
+proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+for dir in $proto_dirs; do
+   protoc \
+  -I "proto" \
   -I="$COSMOS_SDK_DIR/third_party/proto" \
   -I="$COSMOS_SDK_DIR/proto" \
   --gocosmos_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
+
+  # command to generate gRPC gateway (*.pb.gw.go in respective modules) files
+  protoc \
+  -I "proto" \
+  -I="$COSMOS_SDK_DIR/third_party/proto" \
+  -I="$COSMOS_SDK_DIR/proto" \
   --grpc-gateway_out=logtostderr=true:. \
-  --doc_out=./docs \
-  --doc_opt=markdown,proto.md \
-  $(find "${PROJECT_PROTO_DIR}" -maxdepth 1 -name '*.proto')
+  $(find "${dir}" -maxdepth 1 -name '*.proto')
+done
 
 # move proto files to the right places
 cp -r github.com/tendermint/cns/* ./
 rm -rf github.com
-
