d46bf051441d89b3ef2179033c7796aff90ac94c
sahith-narahari <sahithnarahari@gmail.com>
2021-02-24 03:59:56 +0530

    fix lint

--- x/cns/client/rest/rest.go
+++ x/cns/client/rest/rest.go
@@ -1,1 +1,17 @@
 package rest
+
+import (
+	"github.com/gorilla/mux"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	// this line is used by starport scaffolding # 1
+)
+
+const (
+	MethodGet = "GET"
+)
+
+// RegisterRoutes registers cns-related REST handlers to a router
+func RegisterRoutes(clientCtx client.Context, r *mux.Router) {
+	// this line is used by starport scaffolding # 2
+}
