20d898d87e2051de8cef8c689efdf182215a9d45
sahith-narahari <sahithnarahari@gmail.com>
2021-02-28 04:27:25 +0530

    add cli tx

--- x/cns/client/cli/tx.go
+++ x/cns/client/cli/tx.go
@@ -2,7 +2,8 @@ package cli
 
 import (
 	"fmt"
-
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -20,7 +21,78 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(RegisterChainNameCmd())
+	cmd.AddCommand(UpdateChainInfoCmd())
+	return cmd
+}
+
+func RegisterChainNameCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "register",
+		Args:  cobra.ExactArgs(1),
+		Short: "register chain name",
+		Long: `register chain name :
+
+$ <appd> tx cns register <chain-name> flags --from owner
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			ibcClient, _ := cmd.Flags().GetString(FlagCanonicalIbcClient)
+			sourceCodeUrl, _ := cmd.Flags().GetString(FlagSourceCodeUrl)
+			version, _ := cmd.Flags().GetInt64(FlagVersion)
+			commitHash, _ := cmd.Flags().GetString(FlagCommitHash)
+			genesisHash, _ := cmd.Flags().GetString(FlagGenesisHash)
+			seeds, _ := cmd.Flags().GetString(FlagSeeds)
+			owner := clientCtx.GetFromAddress()
+
+			msg := types.NewMsgRegisterChainName(args[0], owner.String(), seeds, sourceCodeUrl,
+				ibcClient, commitHash, genesisHash, version)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	AddCnsTxFlags(cmd)
+	return cmd
+}
+
+func UpdateChainInfoCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update",
+		Args:  cobra.ExactArgs(1),
+		Short: "update chain info",
+		Long: `update chain info :
+
+$ <appd> tx cns update <chain-name> flags --from owner
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			ibcClient, _ := cmd.Flags().GetString(FlagCanonicalIbcClient)
+			sourceCodeUrl, _ := cmd.Flags().GetString(FlagSourceCodeUrl)
+			version, _ := cmd.Flags().GetInt64(FlagVersion)
+			commitHash, _ := cmd.Flags().GetString(FlagCommitHash)
+			genesisHash, _ := cmd.Flags().GetString(FlagGenesisHash)
+			seeds, _ := cmd.Flags().GetString(FlagSeeds)
+			owner := clientCtx.GetFromAddress()
+
+			msg := types.NewMsgUpdateChainInfo(args[0], owner.String(), seeds, sourceCodeUrl,
+				ibcClient, commitHash, genesisHash, version)
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	AddCnsTxFlags(cmd)
 
 	return cmd
 }
--- x/cns/types/msg.go
+++ x/cns/types/msg.go
@@ -1,48 +1,95 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"strings"
+)
 
 var (
 	_ sdk.Msg = &MsgRegisterChainName{}
 	_ sdk.Msg = &MsgUpdateChainInfo{}
 )
 
+const (
+	TypeMsgRegisterChainName = "register_name"
+	TypeMsgUpdateChainInfo   = "update_info"
+)
+
+func NewMsgRegisterChainName(name, owner, seeds, sourceCodeUrl, ibcClient, commitHash, genesisHash string, version int64) *MsgRegisterChainName {
+	return &MsgRegisterChainName{
+		ChainName:          name,
+		Owner:              owner,
+		Seed:               strings.Split(seeds, ","),
+		SourceCodeUrl:      sourceCodeUrl,
+		CanonicalIbcClient: ibcClient,
+		Version: &VersionInfo{
+			Version:          version,
+			SourceCommitHash: commitHash,
+			GenesisHash:      genesisHash,
+		},
+	}
+}
+
 func (m *MsgRegisterChainName) Route() string {
-	panic("implement me")
+	return RouterKey
 }
 
 func (m *MsgRegisterChainName) Type() string {
-	panic("implement me")
+	return TypeMsgRegisterChainName
 }
 
 func (m *MsgRegisterChainName) ValidateBasic() error {
-	panic("implement me")
+	return nil
 }
 
 func (m *MsgRegisterChainName) GetSignBytes() []byte {
-	panic("implement me")
+	bz := ModuleCdc.MustMarshalJSON(m)
+	return sdk.MustSortJSON(bz)
 }
 
 func (m *MsgRegisterChainName) GetSigners() []sdk.AccAddress {
-	panic("implement me")
+	owner, err := sdk.AccAddressFromBech32(m.Owner)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{owner}
 }
 
+func NewMsgUpdateChainInfo(name, owner, seeds, sourceCodeUrl, ibcClient, commitHash, genesisHash string, version int64) *MsgUpdateChainInfo {
+	return &MsgUpdateChainInfo{
+		ChainName:          name,
+		Owner:              owner,
+		Seed:               strings.Split(seeds, ","),
+		SourceCodeUrl:      sourceCodeUrl,
+		CanonicalIbcClient: ibcClient,
+		Version: &VersionInfo{
+			Version:          version,
+			SourceCommitHash: commitHash,
+			GenesisHash:      genesisHash,
+		},
+	}
+}
 func (m *MsgUpdateChainInfo) Route() string {
-	panic("implement me")
+	return RouterKey
 }
 
 func (m *MsgUpdateChainInfo) Type() string {
-	panic("implement me")
+	return TypeMsgUpdateChainInfo
 }
 
 func (m *MsgUpdateChainInfo) ValidateBasic() error {
-	panic("implement me")
+	return nil
 }
 
 func (m *MsgUpdateChainInfo) GetSignBytes() []byte {
-	panic("implement me")
+	bz := ModuleCdc.MustMarshalJSON(m)
+	return sdk.MustSortJSON(bz)
 }
 
 func (m *MsgUpdateChainInfo) GetSigners() []sdk.AccAddress {
-	panic("implement me")
+	owner, err := sdk.AccAddressFromBech32(m.Owner)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{owner}
 }
